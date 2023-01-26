f58668428c5cd5f711be707d1939ac3d9b252cb1
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-01-26 20:52:51 +0530

    Create Test2.java

---
+++ Day 2/Test2.java
@@ -0,0 +1,21 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		char ch = 'A';
+		
+		while(ch<=90) {
+			System.out.print(ch+" ");
+			ch++;
+		}
+		
+		char ch1 = 'A';
+		do {
+			System.out.print(ch+" ");
+			ch++;
+		} while(ch<=90);
+
+	}
+
+}
